7ecef2174bbb011fc6728f9b805c45a873cfddc5
Junhao Zhu <jz5770@nyu.edu>
2025-11-07 22:12:40 +0800

    update sketch

--- particle-world/sketch.js
+++ particle-world/sketch.js
@@ -1,57 +1,132 @@
 // CCLab Mini Project - 9.R Particle World Template
 
-let NUM_OF_PARTICLES = 3; // Decide the initial number of particles.
-let MAX_OF_PARTICLES = 500; // Decide the maximum number of particles.
+let NUM_OF_PARTICLES = 50;
+let MAX_OF_PARTICLES = 500;
 
 let particles = [];
+let currentBaseHue = 0;
+let generationTargetCount = 0;
 
 function setup() {
   let canvas = createCanvas(800, 500);
   canvas.parent("p5-canvas-container");
-
-  // generate particles
-  for (let i = 0; i < NUM_OF_PARTICLES; i++) {
-    particles[i] = new Particle(random(width), random(height));
-  }
+  background(0);
+  frameRate(30);
+  angleMode(DEGREES);
+  ellipseMode(CENTER);
+  colorMode(HSL, 100);
+  currentBaseHue = random(100);
 }
 
 function draw() {
-  background(50);
+  background(0, 0, 0, 4);
+  translate(width / 2, height / 2);
+  particles.sort((a, b) => a.y - b.y);
 
-  // consider generating particles in draw(), using Dynamic Array
-
-  // update and display
-  for (let i = 0; i < particles.length; i++) {
+  for (let i = particles.length - 1; i >= 0; i--) {
     let p = particles[i];
     p.update();
     p.display();
+  
+    if (p.isDead()) {
+      particles.splice(i, 1);
+    }
   }
-
-  // limit the number of particles
-  if (particles.length > MAX_OF_PARTICLES) {
-    particles.splice(0, 1); // remove the first (oldest) particle
+  
+  if (particles.length === 0 && generationTargetCount === 0) {
+    currentBaseHue = random(100);
+    generationTargetCount = NUM_OF_PARTICLES;
+  }
+  
+  if (generationTargetCount > 0 && particles.length < MAX_OF_PARTICLES) {
+    particles.push(new Particle(currentBaseHue));
+    generationTargetCount--;
   }
 }
 
+
 class Particle {
-  // constructor function
-  constructor(startX, startY) {
-    // properties (variables): particle's characteristics
-    this.x = startX;
-    this.y = startY;
-    this.dia = 30;
+  constructor(baseHue) {
+    this.t = random(360);
+    this.speed = 1.5;
+    
+    this.a = width / 3;
+    this.x = 0;
+    this.y = 0;
+
+    this.baseDia = random(10, 60);
+    this.strokeW = random(0.1, 1);
+    
+    this.hue = baseHue + random(-7, 7);
+    if (this.hue < 0) {
+      this.hue += 100;
+    } else if (this.hue > 100) {
+      this.hue -= 100;
+    }
+    this.age = 0;
+    this.maxLifespan = random(150, 250);
+    this.lifeAlpha = 0.0;
+    this.perspectiveScale = 1.0; 
+    this.perspectiveLight = 50;    
+    this.perspectiveStroke = 1.0;  
+    this.finalLightness = 0;       
   }
-  // methods (functions): particle's behaviors
+
   update() {
-    // (add) 
+    this.t += this.speed;
+    this.age += 1;
+    this.a = width / 3; 
+    this.updatePosition();
+    this.updateLifecycle();
+    this.updatePerspective();
+    
+    this.finalLightness = this.perspectiveLight * this.lifeAlpha;
   }
+  
+  updatePosition() {
+    let currentT = this.t;
+    this.x = this.a * sqrt(2) * cos(currentT) / (pow(sin(currentT), 2) + 1);
+    this.y = this.a * sqrt(2) * cos(currentT) * sin(currentT) / (pow(sin(currentT), 2) + 1);
+  }
+  
+  updateLifecycle() {
+    let fadeInDuration = this.maxLifespan * 0.2; 
+    let fadeOutStartTime = this.maxLifespan * 0.7;
+
+    if (this.age < fadeInDuration) {
+      this.lifeAlpha = map(this.age, 0, fadeInDuration, 0, 1);
+    } else if (this.age > fadeOutStartTime) {
+      this.lifeAlpha = map(this.age, fadeOutStartTime, this.maxLifespan, 1, 0);
+    } else {
+      this.lifeAlpha = 1.0;
+    }
+  }
+
+  updatePerspective() {
+    let yRange = this.a / 2;
+    
+    this.perspectiveScale = map(this.y, -yRange, yRange, 0.5, 1.5);
+    this.perspectiveLight = map(this.y, -yRange, yRange, 50, 80);
+    this.perspectiveStroke = map(this.y, -yRange, yRange, this.strokeW * 0.4, this.strokeW * 1.3);
+  }
+  
   display() {
-    // particle's appearance
     push();
     translate(this.x, this.y);
 
-    circle(0, 0, this.dia);
+    noFill();
+    stroke(this.hue, 80, this.finalLightness); 
+    strokeWeight(this.perspectiveStroke);
+
+    let base = this.baseDia * this.perspectiveScale;
+    circle(0, 0, base * 0.3);
+    circle(0, 0, base * 0.8);
+    circle(0, 0, base);
 
     pop();
+  }
+
+  isDead() {
+    return this.age > this.maxLifespan;
   }
 }
