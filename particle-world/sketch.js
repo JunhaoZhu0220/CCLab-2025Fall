f7eeecaa1e4981232af3a6f282968505824913b1
Junhao Zhu <jz5770@nyu.edu>
2025-11-08 15:52:47 +0800

    update bg music and project description

--- particle-world/sketch.js
+++ particle-world/sketch.js
@@ -6,6 +6,11 @@ let MAX_OF_PARTICLES = 500;
 let particles = [];
 let currentBaseHue = 0;
 let generationTargetCount = 0;
+let bgMusic;
+
+function preload() {
+  bgMusic = loadSound('assets/bg-music.wav');
+}
 
 function setup() {
   let canvas = createCanvas(800, 500);
@@ -16,6 +21,8 @@ function setup() {
   ellipseMode(CENTER);
   colorMode(HSL, 100);
   currentBaseHue = random(100);
+  bgMusic.loop();
+  bgMusic.setVolume(0.5);
 }
 
 function draw() {
@@ -27,17 +34,17 @@ function draw() {
     let p = particles[i];
     p.update();
     p.display();
-  
+
     if (p.isDead()) {
       particles.splice(i, 1);
     }
   }
-  
+
   if (particles.length === 0 && generationTargetCount === 0) {
     currentBaseHue = random(100);
     generationTargetCount = NUM_OF_PARTICLES;
   }
-  
+
   if (generationTargetCount > 0 && particles.length < MAX_OF_PARTICLES) {
     particles.push(new Particle(currentBaseHue));
     generationTargetCount--;
@@ -49,14 +56,14 @@ class Particle {
   constructor(baseHue) {
     this.t = random(360);
     this.speed = 1.5;
-    
+
     this.a = width / 3;
     this.x = 0;
     this.y = 0;
 
     this.baseDia = random(10, 60);
     this.strokeW = random(0.1, 1);
-    
+
     this.hue = baseHue + random(-7, 7);
     if (this.hue < 0) {
       this.hue += 100;
@@ -66,31 +73,31 @@ class Particle {
     this.age = 0;
     this.maxLifespan = random(150, 250);
     this.lifeAlpha = 0.0;
-    this.perspectiveScale = 1.0; 
-    this.perspectiveLight = 50;    
-    this.perspectiveStroke = 1.0;  
-    this.finalLightness = 0;       
+    this.perspectiveScale = 1.0;
+    this.perspectiveLight = 50;
+    this.perspectiveStroke = 1.0;
+    this.finalLightness = 0;
   }
 
   update() {
     this.t += this.speed;
     this.age += 1;
-    this.a = width / 3; 
+    this.a = width / 3;
     this.updatePosition();
     this.updateLifecycle();
     this.updatePerspective();
-    
+
     this.finalLightness = this.perspectiveLight * this.lifeAlpha;
   }
-  
+
   updatePosition() {
     let currentT = this.t;
     this.x = this.a * sqrt(2) * cos(currentT) / (pow(sin(currentT), 2) + 1);
     this.y = this.a * sqrt(2) * cos(currentT) * sin(currentT) / (pow(sin(currentT), 2) + 1);
   }
-  
+
   updateLifecycle() {
-    let fadeInDuration = this.maxLifespan * 0.2; 
+    let fadeInDuration = this.maxLifespan * 0.2;
     let fadeOutStartTime = this.maxLifespan * 0.7;
 
     if (this.age < fadeInDuration) {
@@ -104,18 +111,18 @@ class Particle {
 
   updatePerspective() {
     let yRange = this.a / 2;
-    
+
     this.perspectiveScale = map(this.y, -yRange, yRange, 0.5, 1.5);
     this.perspectiveLight = map(this.y, -yRange, yRange, 50, 80);
     this.perspectiveStroke = map(this.y, -yRange, yRange, this.strokeW * 0.4, this.strokeW * 1.3);
   }
-  
+
   display() {
     push();
     translate(this.x, this.y);
 
     noFill();
-    stroke(this.hue, 80, this.finalLightness); 
+    stroke(this.hue, 80, this.finalLightness);
     strokeWeight(this.perspectiveStroke);
 
     let base = this.baseDia * this.perspectiveScale;
